d2e1a016bda7e36b5a30197c393299d60eac0b8a
luisarteagadev <l.angelarteaga713@gmail.com>
2024-11-22 00:05:56 -0500

    diseño product page

--- src/pages/ProductPage.vue
+++ src/pages/ProductPage.vue
@@ -1,168 +1,79 @@
 <template>
-  <header class="ht-header">
+  <header>
     <div class="container">
-      <nav class="navbar navbar-default navbar-custom">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header logo">
-          <div
-            class="navbar-toggle"
-            data-toggle="collapse"
-            data-target="#bs-example-navbar-collapse-1"
-          >
-            <span class="sr-only">Toggle navigation</span>
-            <div id="nav-icon1">
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-          <a href="index-2.html"
-            ><img
-              class="logo"
-              src="images/logo1.png"
-              alt=""
-              width="119"
-              height="58"
-          /></a>
-        </div>
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div
-          class="collapse navbar-collapse flex-parent"
-          id="bs-example-navbar-collapse-1"
-        >
-          <ul class="nav navbar-nav flex-child-menu menu-left">
-            <li class="hidden">
-              <a href="#page-top"></a>
-            </li>
-            <li class="dropdown first">
-              <a
-                class="btn btn-default dropdown-toggle lv1"
-                data-toggle="dropdown"
-              >
-                Home <i class="fa fa-angle-down" aria-hidden="true"></i>
-              </a>
-              <ul class="dropdown-menu level1">
-                <li><a href="index-2.html">Home 01</a></li>
-                <li><a href="homev2.html">Home 02</a></li>
-                <li><a href="homev3.html">Home 03</a></li>
-              </ul>
-            </li>
-            <li class="dropdown first">
-              <a
-                class="btn btn-default dropdown-toggle lv1"
-                data-toggle="dropdown"
-                data-hover="dropdown"
-              >
-                movies<i class="fa fa-angle-down" aria-hidden="true"></i>
-              </a>
-              <ul class="dropdown-menu level1">
-                <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown"
-                    >Movie grid<i class="ion-ios-arrow-forward"></i
-                  ></a>
-                  <ul class="dropdown-menu level2">
-                    <li><a href="moviegrid.html">Movie grid</a></li>
-                    <li>
-                      <a href="moviegridfw.html">movie grid full width</a>
-                    </li>
-                  </ul>
-                </li>
-                <li><a href="movielist.html">Movie list</a></li>
-                <li><a href="moviesingle.html">Movie single</a></li>
-                <li class="it-last">
-                  <a href="seriessingle.html">Series single</a>
-                </li>
-              </ul>
-            </li>
-            <li class="dropdown first">
-              <a
-                class="btn btn-default dropdown-toggle lv1"
-                data-toggle="dropdown"
-                data-hover="dropdown"
-              >
-                celebrities <i class="fa fa-angle-down" aria-hidden="true"></i>
-              </a>
-              <ul class="dropdown-menu level1">
-                <li><a href="celebritygrid01.html">celebrity grid 01</a></li>
-                <li><a href="celebritygrid02.html">celebrity grid 02 </a></li>
-                <li><a href="celebritylist.html">celebrity list</a></li>
-                <li class="it-last">
-                  <a href="celebritysingle.html">celebrity single</a>
-                </li>
-              </ul>
-            </li>
-            <li class="dropdown first">
-              <a
-                class="btn btn-default dropdown-toggle lv1"
-                data-toggle="dropdown"
-                data-hover="dropdown"
-              >
-                news <i class="fa fa-angle-down" aria-hidden="true"></i>
-              </a>
-              <ul class="dropdown-menu level1">
-                <li><a href="bloglist.html">blog List</a></li>
-                <li><a href="bloggrid.html">blog Grid</a></li>
-                <li class="it-last">
-                  <a href="blogdetail.html">blog Detail</a>
-                </li>
-              </ul>
-            </li>
-            <li class="dropdown first">
-              <a
-                class="btn btn-default dropdown-toggle lv1"
-                data-toggle="dropdown"
-                data-hover="dropdown"
-              >
-                community <i class="fa fa-angle-down" aria-hidden="true"></i>
-              </a>
-              <ul class="dropdown-menu level1">
-                <li><a href="userfavoritegrid.html">user favorite grid</a></li>
-                <li><a href="userfavoritelist.html">user favorite list</a></li>
-                <li><a href="userprofile.html">user profile</a></li>
-                <li class="it-last"><a href="userrate.html">user rate</a></li>
-              </ul>
-            </li>
-          </ul>
-          <ul class="nav navbar-nav flex-child-menu menu-right">
-            <li class="dropdown first">
-              <a
-                class="btn btn-default dropdown-toggle lv1"
-                data-toggle="dropdown"
-                data-hover="dropdown"
-              >
-                pages <i class="fa fa-angle-down" aria-hidden="true"></i>
-              </a>
-              <ul class="dropdown-menu level1">
-                <li><a href="landing.html">Landing</a></li>
-                <li><a href="404.html">404 Page</a></li>
-                <li class="it-last">
-                  <a href="comingsoon.html">Coming soon</a>
-                </li>
-              </ul>
-            </li>
-            <li><a href="#">Help</a></li>
-            <li class="loginLink"><a href="#">LOG In</a></li>
-            <li class="btn signupLink"><a href="#">sign up</a></li>
-          </ul>
-        </div>
-        <!-- /.navbar-collapse -->
-      </nav>
-
-      <!-- top search form -->
-      <div class="top-search">
-        <select>
-          <option value="united">TV show</option>
-          <option value="saab">Others</option>
-        </select>
-        <input
-          type="text"
-          placeholder="Search for a movie, TV Show or celebrity that you are looking for"
-        />
-      </div>
+      <div id="logo">SHOWMOVIES</div>
     </div>
   </header>
+
+  <section class="content">
+    <div class="container" style="margin-top: 10px">
+      <h1>Welcome to Our Website!</h1>
+      <p>Get plenty of knowledge by our wonderful courses.</p>
+    </div>
+    <div class="container" style="margin-top: 10px">
+      <div class="product-page">
+        <div class="product-filter q-ml-md q-mr-xl">
+          <ProductFilter @categoriaCambiada="actualizarCategoria" />
+        </div>
+        <div class="product-list">
+          <ProductList :categoriaFiltrada="categoriaSeleccionada" />
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
-<style></style>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 20px;
+  overflow: hidden;
+}
 
-<script></script>
+header {
+  background-color: #333;
+  color: #fff;
+  padding: 10px 0;
+}
+
+#logo {
+  float: left;
+  font-size: 1.5em;
+}
+
+.content {
+  padding: 50px 0;
+  text-align: center;
+}
+
+.product-page {
+  display: flex;
+  justify-content: space-between;
+}
+
+.product-filter {
+  width: 25%;
+}
+
+.product-list {
+  width: 75%;
+}
+</style>
+
+<script>
+export default {
+  name: "ProductPage",
+  components: {},
+  data() {
+    return {
+      categoriaSeleccionada: null,
+    };
+  },
+};
+</script>
